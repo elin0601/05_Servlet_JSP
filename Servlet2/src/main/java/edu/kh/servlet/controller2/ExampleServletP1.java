f9095c3fc5c59242f289ecd8e7779fd5b799840e
dlwltn <03dreams@naver.com>
2024-02-20 16:50:45 +0900

    240220_피자 주문 내역

--- Servlet2/src/main/java/edu/kh/servlet/controller2/ExampleServletP1.java
+++ Servlet2/src/main/java/edu/kh/servlet/controller2/ExampleServletP1.java
@@ -12,6 +12,23 @@ public class ExampleServletP1 extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		String pirimumPizza = req.getParameter("pirimumPizza");
+		String pizza = req.getParameter("pizza");
+		
+		String[]  topping = req.getParameterValues("topping");
+		String[]  opt = req.getParameterValues("opt");
+		
+		String orderName = req.getParameter("orderName");
+		String orderAddress = req.getParameter("orderAddress");
+		String orderRequest = req.getParameter("orderRequest");
+		
+		System.out.println("피자먹고싶네");
+		
+		int price = 0;
+	
+		
+		
+		
 	}
 
 }
