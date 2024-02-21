8b169245565d1f970c878da2685b2d753bd90a96
dlwltn <03dreams@naver.com>
2024-02-21 17:26:33 +0900

    240221_Servlet, JSP

---
+++ JSP1/src/main/webapp/resources/js/pizza.js
@@ -0,0 +1,27 @@
+console.log("pizza.js 파일이 요청됨");
+
+/* const plusBtn = document.querySelector(".row button:nth-of-type(2)");
+const minusBtn = document.querySelector(".row button:nth-of-type(1)");
+
+const input = document.querySelector(".row input");
+
+plusBtn.addEventListener(click, function () {
+
+    const amount = parseInt(input.amount);
+
+    if (amount < parseInt(input.getAttribute('max'))) {
+
+        input.amount = amount++;
+    }
+});
+
+minusBtn.addEventListener(click, function () {
+
+    const amount = parseInt(input.amount);
+
+    if (amount > parseInt(input.getAttribute('min'))) {
+
+        input.amount = amount--;
+    }
+});
+ */
